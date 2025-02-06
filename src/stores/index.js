198150e3e1e1fc750c5ba5093d5ea6367838a725
xx572959496 <572959496@qq.com>
2025-02-06 14:13:39 +0800

    bugs fix

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,26 +2,32 @@ import {defineStore} from "pinia";
 import {computed, ref} from "vue";
 
 // setup() 函数写法
-export const useLessonsListStore = defineStore('email', () => {
-    const lessonsList = ref([{
-        id:'',
-        title:'',
-        provenance:'',
-        author: {
-            name:'',
+export const useLessonsListStore = defineStore(
+    'email',
+    () => {
+        const lessonsList = ref([{
+            id: '',
+            title: '',
+            provenance: '',
+            author: {
+                name: '',
+            },
+            article: '',
+            comment_count: null,
+            favourite_count: null,
+            commentsList: [],
+            randomCommentList: [],
+            date_by_day: ''
+        }]); // state
+        const getterLessons = computed(() => { // getters
+            return lessonsList.value;
+        })
+        const changeLessonsList = (newLessonsList) => { // actions
+            lessonsList.value = newLessonsList
+        }
+        return {lessonsList, getterLessons, changeLessonsList}
+    }, {
+        persist: {
+            storage: sessionStorage
         },
-        article:'',
-        comment_count:null,
-        favourite_count:null,
-        commentsList:[],
-        randomCommentList:[],
-        date_by_day:''
-    }]); // state
-    const getterLessons = computed(() => { // getters
-        return lessonsList.value;
     })
-    const changeLessonsList = (newLessonsList) => { // actions
-        lessonsList.value = newLessonsList
-    }
-    return {lessonsList, getterLessons, changeLessonsList}
-})
